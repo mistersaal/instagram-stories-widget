12a3d7bb309582b646867cb58e043452474547d2
Сааль Степан <mister.saal@yandex.ru>
2020-04-03 13:49:30 +0300

    Исправление небольшого недочёта в HomeController.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 class HomeController extends Controller
 {
@@ -23,7 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $highlights = auth()->user()->highlights;
+        $highlights = auth()->user()->highlights ?? new Collection();
         return view('home')->with(compact('highlights'));
     }
 }
